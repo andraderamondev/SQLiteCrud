3b190134bb534e6a8ca43b737b4e90cade6c730d
Ramon Dev <andraderamondev@gmail.com>
2017-09-22 12:26:36 -0300

    Correção de views e functions.

--- app/src/main/java/com/dev/ramon/sqlitecrud/activities/CourseDetailActivity.java
+++ app/src/main/java/com/dev/ramon/sqlitecrud/activities/CourseDetailActivity.java
@@ -1,10 +1,14 @@
 package com.dev.ramon.sqlitecrud.activities;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.dev.ramon.sqlitecrud.R;
@@ -12,21 +16,12 @@ import com.dev.ramon.sqlitecrud.objects.Course;
 
 public class CourseDetailActivity extends AppCompatActivity {
     private Course course;
+    private AlertDialog alert;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         getValuesExtras(getIntent().getExtras());
         toolbar.setTitle(course.getNome());
@@ -36,5 +31,51 @@ public class CourseDetailActivity extends AppCompatActivity {
 
     private void getValuesExtras(Bundle extras) {
         course = extras.getParcelable("course");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_delete) {
+            showDialog();
+            return true;
+        }
+        if (id == R.id.action_edit) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void showDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Atenção");
+        builder.setMessage("Deseja realmente excluir esse curso?");
+        builder.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+                //
+            }
+        });
+        builder.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+                //
+            }
+        });
+        alert = builder.create();
+        alert.show();
+        alert.getButton(alert.BUTTON_NEGATIVE).setTextColor(getResources().getColor(R.color.colorPrimary));
+        alert.getButton(alert.BUTTON_POSITIVE).setTextColor(getResources().getColor(R.color.colorPrimary));
     }
 }
